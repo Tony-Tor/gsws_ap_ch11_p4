b283e3332b2dacd07f3ae5c91820d7c056d7f663
TonyTor <Tony.Tor2016@yandex.ru>
2023-08-22 16:51:10 +0400

    Initial commit

---
+++ src/main/java/com/example/gsws_ap_ch11_p4/GswsApCh11P4Application.java
@@ -0,0 +1,13 @@
+package com.example.gsws_ap_ch11_p4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GswsApCh11P4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GswsApCh11P4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gsws_ap_ch11_p4/GswsApCh11P4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsws_ap_ch11_p4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GswsApCh11P4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
